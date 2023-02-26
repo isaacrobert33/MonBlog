88c084c0dca3da4e6bcf91e1b20964b98ea925cd
Isaac Robert <isaac@sloovi.com>
2023-02-26 18:33:33 +0100

    Extending the blog app

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def main_page(request):
+
+    return render(
+        request,
+        "main/page/main.html",
+        {}
+    )
